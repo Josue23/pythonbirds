e4e23a90b76bed7eaf278beaa0cea23855c0e043
josue23 <josuerodrigues@gmail.com>
2019-07-21 19:05:32 -0300

    Alterada a classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,2 @@
 class Pessoa:
-    def cumprimentar(self):
-        return f'Olá id(self): {id(self)}'
-
-
-if __name__ == '__main__':
-    maria = Pessoa()
-    print(maria.cumprimentar())
-    print('id(self) e id(maria) são iguais.')
-    print(f'id(maria): {id(maria)}')
+    pass
